a928e96d8d03843fa904b88dcbd415104c4cb241
Abhishek <abhishekverma7t@gmail.com>
2024-12-16 04:25:33 +0530

    flutterfire setup

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,14 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_vroom/firebase_options.dart';
 
-void main(){
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
   runApp(MyApp());
 }
 
@@ -15,9 +23,9 @@ class MyApp extends StatelessWidget {
       home: Scaffold(
         appBar: AppBar(),
         body: Center(
-          child: Text('Hello, App!'),
+          child: Text('Hello, Appp!'),
         ),
-      )
+      ),
     );
   }
 }
