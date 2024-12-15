ad8eef6d07eb57bf6796ca8884e9c0a0172f5ab2
Abhishek <abhishekverma7t@gmail.com>
2024-12-16 03:59:39 +0530

    inital commit

---
+++ android/app/src/main/kotlin/com/example/flutter_vroom/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.flutter_vroom
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+void main(){
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'VroomVroom',
+      home: Scaffold(
+        appBar: AppBar(),
+        body: Center(
+          child: Text('Hello, App!'),
+        ),
+      )
+    );
+  }
+}
